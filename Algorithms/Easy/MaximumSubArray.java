02cb32aad13fd093df7fe7cca920eb8d8f65aacc
Kikiodazie <kiki.odazie@yahoo.com>
2021-04-30 01:32:52 +0100

    [LeetCode] MaximumSubArray

---
+++ Algorithms/Easy/MaximumSubArray.java
@@ -0,0 +1,16 @@
+public class MaximumSubArray {
+
+    public static int maxSubArray(int[] nums) {
+
+        int maxSub = nums[0], curSum = 0;
+
+        for (int n : nums) {
+            if (curSum < 0)
+                curSum = 0;
+            curSum += n;
+            maxSub = Math.max(maxSub, curSum);
+        }
+        return maxSub;
+
+    }
+}
